2ca8710a55143c1ce4907069e8ac36712fbcfa10
haleemah-163 <abolaimon777@gmail.com>
2020-12-23 15:17:29 +0200

    myfirstdynamicpaaaaaaaaage

---
+++ app.js
@@ -0,0 +1,12 @@
+// here is my first Activation for my web page with JavaScript
+var age = prompt('your age dear?');
+var message ;
+var sorry = 'sorry and good luck'
+if (age<20) {
+    var message = 'quaified';
+    alert (message)
+    
+} else { alert (sorry)
+    
+}
+
