6ad6abab510f8b5bb95da29c7ecacf1eac7cb361
haleemah-163 <abolaimon777@gmail.com>
2020-12-24 18:47:10 +0200

    break مشان الله

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 // here is my first Activation for my web page with JavaScript
 var age = prompt('your age dear?');
-var message ;
+var message = 'quaified';
 var sorry = 'sorry and good luck'
 if (age<20) {
-    var message = 'quaified';
+    
     alert (message)
     
 } else { alert (sorry)
     
 }
 
+// here is my first editing for my dynamic web page to add  lab8
+
+var name1 = prompt ('now  your name sweetie');
+while( name1 === '' || name1 === null){ 
+    name1 = prompt ('oops it seems you forgot to enter your name or what?');
+}
+
+
+document.write( '< img src="https://financialtribune.com/sites/default/files/field/image/17january/05_china_280-ed.jpg" >') ;
+
+
+ // var energy = prompt ('how would you agree with renewable energy, from 1-10');
+//while (energy > 7 ) {
+    
+//}
+//document.write()
